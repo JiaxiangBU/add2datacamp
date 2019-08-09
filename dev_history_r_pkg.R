a3110479f4cbbca4dbc58e6dac0da2b750bae27b
Jiaxiang Li <alex.lijiaxiang@foxmail.com>
2019-08-09 19:19:40 +0800

    add dev_history

---
+++ dev_history_r_pkg.R
@@ -0,0 +1,85 @@
+# The latest one is always updated, copy back.
+# setup -------------------------------------------------------------------
+
+
+
+
+library(devtools)
+use_git()
+
+# https://github.com/JiaxiangBU/add2impala/blob/master/DESCRIPTION
+file.edit("DESCRIPTION")
+library(devtools)
+
+use_build_ignore("dev_history.R")
+use_roxygen_md()
+use_pipe()
+library(magrittr)
+
+options(usethis.full_name = "Jiaxiang Li")
+use_mit_license()
+
+
+# coding ------------------------------------------------------------------
+
+file.edit("R/xgb_sql_score_query.R")
+file.edit("R/xgb_tree_sql.R")
+# add title
+
+# prettify ----------------------------------------------------------------
+
+use_readme_rmd()
+# help translate XGBoost model R object into SQL statement.
+file.edit("DESCRIPTION")
+rmarkdown::render("README.Rmd")
+file.remove("README.html")
+
+
+# build -------------------------------------------------------------------
+
+document()
+# load_all()
+install()
+
+
+# commit
+
+# release -----------------------------------------------------------------
+
+use_news_md()
+use_version()
+
+
+# add disclaimer ----------------------------------------------------------
+
+file.edit("DESCRIPTION")
+clipr::write_clip('`r add2pkg::add_disclaimer("Roland Stevenson;Jiaxiang Li")`')
+file.edit("README.Rmd")
+rmarkdown::render("README.Rmd")
+rstudioapi::viewer("README.html")
+file.remove("README.html")
+usethis::use_code_of_conduct()
+
+
+# publish -----------------------------------------------------------------
+
+# push
+# make public
+use_github_release()
+# publish release
+# https://zenodo.org/account/settings/github/
+
+
+# add badge and citation --------------------------------------------------
+
+# login zenodo and copy badge in markdown
+file.edit("README.Rmd")
+clipr::write_clip('## Citations
+
+```{r}
+citations <- add2pkg::add_zenodo_citation("README.Rmd")
+```')
+# 需要等一段时间，有时候 doi 没有显示出来
+rmarkdown::render("README.Rmd")
+rstudioapi::viewer("README.html")
+file.remove("README.html")
