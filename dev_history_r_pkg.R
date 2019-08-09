2fd72dc967c9d9f1050707f1ea3d892fe56ba0ea
Jiaxiang Li <alex.lijiaxiang@foxmail.com>
2019-08-10 01:51:57 +0800

    update

--- dev_history_r_pkg.R
+++ dev_history_r_pkg.R
@@ -79,10 +79,35 @@ use_github_release()
 file.edit("README.Rmd")
 clipr::write_clip('## Citations
 
-```{r}
+```{r include=FALSE}
 citations <- add2pkg::add_zenodo_citation("README.Rmd")
+```
+
+```{r echo=FALSE, results=\'asis\'}
+cat(citations$Cite)
+```
+
+```{r echo=FALSE, results=\'asis\'}
+cat(paste0("```BibTex\\n",citations$BibTex,"\\n```"))
+```
+
+```{r echo=FALSE, results=\'asis\'}
+cat(citations$Comments)
 ```')
 # 需要等一段时间，有时候 doi 没有显示出来
 rmarkdown::render("README.Rmd")
 rstudioapi::viewer("README.html")
-file.remove("README.html")
+safely(file.remove)("README.html")
+
+
+# add vignette ------------------------------------------------------------
+
+use_vignette("xgb_to_sql")
+
+
+# pkgdown -----------------------------------------------------------------
+
+start_time <- lubridate::now()
+pkgdown::build_site()
+end_time <- lubridate::now()
+end_time - start_time
